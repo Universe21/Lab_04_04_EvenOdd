dcbdf855dc3be17cac3b08ef84056588e902d2fa
Universe21 <leachhs@mail.uc.edu>
2023-09-18 23:56:24 -0400

    Project into

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Hard code the value for the input as a variable instantiation
+        int number = 8;
+
+// Use the modulo function to indicate if the number is odd or even
+        if (number % 2 == 0) {
+            System.out.println("The number is even.");
+        } else {
+            System.out.println("The number is odd.");
+        }
+    }
+}
+
+
